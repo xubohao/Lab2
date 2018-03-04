e7d8bcb98394c9a8e0fd2fece06be5d80e820980
xubohao <36509716+xubohao@users.noreply.github.com>
2018-03-04 16:37:56 +0800

    Update Hand.java

--- FlamingoBLL/src/main/java/pkgCore/Hand.java
+++ FlamingoBLL/src/main/java/pkgCore/Hand.java
@@ -3,6 +3,8 @@ package pkgCore;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import pkgEnum.eRank;
+
 public class Hand {
 
 	private int iScore;
@@ -17,22 +19,48 @@ public class Hand {
 	{
 		int [] iScore = new int[2];
 		
-		iScore[0] = 5;
-		iScore[1] = 10;
+		iScore[0] = 0;
+		iScore[1] = 0;
 		
 		Collections.sort(cards);
 		
-		
+		//	DONE: Determine the score.  
+		//			Cards:
+		//			2-3-4 - score = 11
+		//			5-J-Q - score = 25
+		//			5-6-7-2 - score = 20
+		//			J-Q	- score = 20
+		//			8-A = score = 9 or 19
+		//			4-A = score = 5 or 15
+		int numOfAce = 0;
 		for (Card c: cards)
 		{
-			//	TODO: Determine the score.  
-			//			Cards:
-			//			2-3-4 - score = 11
-			//			5-J-Q - score = 25
-			//			5-6-7-2 - score = 20
-			//			J-Q	- score = 20
-			//			8-A = score = 9 or 19
-			//			4-A = score = 5 or 15
+			if (c.geteRank() == eRank.ACE) {
+				// for Ace:
+				numOfAce += 1;
+				iScore[0] += 11;
+				iScore[1] += 11;
+			} 
+			else if (c.geteRank().getiRankNbr() >= eRank.JACK.getiRankNbr()) {
+				// for Face:
+				iScore[0] += 10;
+				iScore[1] += 10;
+			}
+			else {
+				iScore[0] += c.geteRank().getiRankNbr();
+				iScore[1] += c.geteRank().getiRankNbr();
+			}
+		}
+		if (numOfAce >= 1) {
+			while (iScore[1] > 21 && numOfAce > 0) {
+				iScore[1] -= 10;
+				numOfAce -= 1;
+			}
+			if (numOfAce > 0 && iScore[1] != 21) {
+				iScore[0] = iScore[1] - 10;
+			} else {
+				iScore[0] = iScore[1];
+			}
 		}
 		
 		return iScore;
@@ -40,7 +68,8 @@ public class Hand {
 	
 	public void Draw(Deck d)
 	{
-		//	TODO: add a card to 'cards' from a card drawn from Deck d
+		//	DONE: add a card to 'cards' from a card drawn from Deck d
+		cards.add(d.draw());
 	}
 	
 	private void AddCard(Card c)
