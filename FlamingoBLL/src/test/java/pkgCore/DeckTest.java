32987340972174e5fb306f46b29673e9500b8950
xubohao <36509716+xubohao@users.noreply.github.com>
2018-03-04 16:38:23 +0800

    Update DeckTest.java

--- FlamingoBLL/src/test/java/pkgCore/DeckTest.java
+++ FlamingoBLL/src/test/java/pkgCore/DeckTest.java
@@ -10,7 +10,12 @@ public class DeckTest {
 	public void TestDeck() {
 		
 		//	TODO: Build a deck(1), make sure there are 52 cards in the deck
+		Deck d_1 = new Deck(1);
+		assertEquals(d_1.getNumOfCards(), 52);	
+		
 		//	TODO: Build a deck(6), make sure there are 312 cards in the deck 
+		Deck d_2 = new Deck(6);
+		assertEquals(d_2.getNumOfCards(), 312);
 	}
 
 }
